e831c36bc5fa90caf95be8a28a03ecc4d3802a28
peichan <ktgchyshnr13@gmail.com>
2014-09-17 15:37:15 +0900

    ゲームクリア時にリトライ等を選択できるようにした

--- UnityChanEscape/Assets/Scripts/StageObject/GameClear.cs
+++ UnityChanEscape/Assets/Scripts/StageObject/GameClear.cs
@@ -3,29 +3,78 @@ using System.Collections;
 
 public class GameClear : MonoBehaviour {
 
-	public GUIText clearText;
+	public GUIText[] allTexts;
+	public GUIText arrow;
+	private int selectedText;
+	private int maxTextNum;
 	public AudioSource clearVoice;
 	private bool is_cleared = false;
 
 	GameObject TimerObject;
 
 	// Use this for initialization
-	void Start () {
-		TimerObject = GameObject.Find ("TimeManager");
-		clearText.enabled = false;
+	void Start ()
+	{
+			TimerObject = GameObject.Find ("TimeManager");
+			foreach (GUIText gt in allTexts) {
+						gt.enabled = false;
+			}
+			selectedText = 0;
+			maxTextNum = 1;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (is_cleared && Input.GetKeyDown ("z")) {
-			Application.LoadLevel("StageSelect");
-		}
+	void Update ()
+	{
+			//クリア時の処理
+			if (is_cleared) {
+						
+					// 上キー
+					if (Input.GetKeyDown ("up")) {
+							if (selectedText > 0) {
+									selectedText -= 1;
+							} else {
+									selectedText = maxTextNum;
+							}
+
+					}
+	
+					// 下キー
+					if (Input.GetKeyDown ("down")) {
+							if (selectedText < maxTextNum) {
+									selectedText += 1;
+							} else {
+									selectedText = 0;
+							}
+
+					}
+					
+					// 矢印の位置を変更
+					arrow.transform.position = new Vector3 (0.25f, 0.5f - 0.1f * selectedText, 0f);
+					
+					// zキー
+					if (Input.GetKeyDown ("z")) {
+							switch (selectedText) {
+							case 0:
+									Application.LoadLevel (Application.loadedLevel);
+									Time.timeScale = 1f;
+									break;
+							case 1:
+									Application.LoadLevel ("StageSelect");
+									Time.timeScale = 1f;
+									break;
+							}
+					}
+			}
 	}
 
-	void Cleared () {
-		TimerObject.SendMessage ("Stop");
-		clearText.enabled = true;
-		clearVoice.Play();
-		is_cleared = true;
+	void Cleared ()
+	{
+			TimerObject.SendMessage ("Stop");
+			foreach (GUIText gt in allTexts) {
+						gt.enabled = true;
+			}
+			clearVoice.Play();
+			is_cleared = true;
 	}
 }
