a3d496e3ee6971e9690993579b0e7042b5c2be8f
peichan <ktgchyshnr13@gmail.com>
2014-09-27 20:03:14 +0900

    箱のコライダーを拡張、キー入力時のみ箱を押すアニメーションを再生するようにした

--- UnityChanEscape/Assets/Scripts/StageObject/BoxController.cs
+++ UnityChanEscape/Assets/Scripts/StageObject/BoxController.cs
@@ -40,15 +40,15 @@ public class BoxController : MonoBehaviour
 		
 		}
 				
-		void OnCollisionStay(Collision col) 
+		void OnCollisionStay (Collision col)
 		{
-			Debug.Log("ontriggerenter");
+				Debug.Log ("ontriggerenter");
 
-			double error = collisionError;
+				double error = collisionError;
 		
-			if (col.gameObject.tag == "Player") {
+				if (col.gameObject.tag == "Player") {
 
-				ContactPoint contact = col.contacts[0];
+						ContactPoint contact = col.contacts [0];
 
 //				foreach (ContactPoint c in col.contacts) {
 //					
@@ -56,37 +56,46 @@ public class BoxController : MonoBehaviour
 //
 //				}
 							
-				bool canPushForward = System.Math.Abs (contact.point.x - transform.position.x) <= collisionWidth && System.Math.Abs (contact.point.z - (transform.position.z + 0.5)) <= error;
-				bool canPushBack = System.Math.Abs (contact.point.x - transform.position.x) <= collisionWidth && System.Math.Abs (contact.point.z - (transform.position.z - 0.5)) <= error;
-				bool canPushLeft = System.Math.Abs (contact.point.z - transform.position.z) <= collisionWidth && System.Math.Abs (contact.point.x - (transform.position.x - 0.5)) <= error;
-				bool canPushRight = System.Math.Abs (contact.point.z - transform.position.z) <= collisionWidth && System.Math.Abs (contact.point.x - (transform.position.x + 0.5)) <= error;
+					bool canPushForward = System.Math.Abs (contact.point.x - transform.position.x) <= collisionWidth && System.Math.Abs (contact.point.z - (transform.position.z + 0.5)) <= error;
+					bool canPushBack = System.Math.Abs (contact.point.x - transform.position.x) <= collisionWidth && System.Math.Abs (contact.point.z - (transform.position.z - 0.5)) <= error;
+					bool canPushLeft = System.Math.Abs (contact.point.z - transform.position.z) <= collisionWidth && System.Math.Abs (contact.point.x - (transform.position.x - 0.5)) <= error;
+					bool canPushRight = System.Math.Abs (contact.point.z - transform.position.z) <= collisionWidth && System.Math.Abs (contact.point.x - (transform.position.x + 0.5)) <= error;
 
+					col.gameObject.GetComponent<Animator> ().SetBool ("isPushing", true);
+					
+					if (canPushForward) {
+							//if (contact.point.x >= (transform.position.x - collisionWidth) && contact.point.x <= (transform.position.x + collisionWidth) && System.Math.Abs(contact.point.z - (transform.position.z + 0.5)) <= error) {
+							//print ((System.Math.Abs (contact.point.z - (transform.position.z + 0.5)) <= error)? 1: 0);
+							//Debug.Log ("can push");
 
-				if (canPushForward) {
-						//if (contact.point.x >= (transform.position.x - collisionWidth) && contact.point.x <= (transform.position.x + collisionWidth) && System.Math.Abs(contact.point.z - (transform.position.z + 0.5)) <= error) {
-						//print ((System.Math.Abs (contact.point.z - (transform.position.z + 0.5)) <= error)? 1: 0);
-						//Debug.Log ("can push");
+							// TODO: change unity-chan's pose
 
-						// TODO: change unity-chan's pose
+							transform.Translate (0, 0, -1 * Time.deltaTime);
 
-						transform.Translate (0, 0, -1 * Time.deltaTime);
+					} else if (canPushBack) {
 
-				} else if (canPushBack) {
+							transform.Translate (0, 0, 1 * Time.deltaTime);
 
-						transform.Translate (0, 0, 1 * Time.deltaTime);
+					} else if (canPushLeft) {
 
-				} else if (canPushLeft) {
+							transform.Translate (1 * Time.deltaTime, 0, 0);
 
-						transform.Translate (1 * Time.deltaTime, 0, 0);
+					} else if (canPushRight) {
 
-				} else if (canPushRight) {
+							transform.Translate (-1 * Time.deltaTime, 0, 0);
 
-						transform.Translate (-1 * Time.deltaTime, 0, 0);
-
-				}
-
-				
+					}
+					if (!(Input.GetKey ("up") || Input.GetKey ("down") || Input.GetKey ("right") || Input.GetKey ("left"))) {
+								col.gameObject.GetComponent<Animator>().SetBool("isPushing", false);
+					}
+			
 			}
 		}
-				
+		
+		void OnCollisionExit (Collision col)
+		{
+				if (col.gameObject.tag == "Player") {
+						col.gameObject.GetComponent<Animator> ().SetBool ("isPushing", false);
+				}
+		}		
 }
