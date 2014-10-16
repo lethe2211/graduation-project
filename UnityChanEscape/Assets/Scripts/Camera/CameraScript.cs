34eac2bd22717c5a67eb2975b991a12a8bce2d9e
peichan <ktgchyshnr13@gmail.com>
2014-10-16 18:12:37 +0900

    主観カメラを実装

--- UnityChanEscape/Assets/Scripts/Camera/CameraScript.cs
+++ UnityChanEscape/Assets/Scripts/Camera/CameraScript.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public class CameraScript : MonoBehaviour {
 
-		Camera mainCamera;//メインカメラ
-		Camera subCamera;//サブのカメラです
+		Camera mainCamera; // メインカメラ
+		Camera subCamera; // サブのカメラです
 		GameObject unityChan;
 		GameObject boxUnityChan;
+		bool isFirstPersonCamera; // 主眼カメラを使用中かどうか
+		private Vector3 defaultEulerAngles;
 
 		// Use this for initialization
 		void Start () {
@@ -15,11 +17,31 @@ public class CameraScript : MonoBehaviour {
 				unityChan = GameObject.Find("unitychan");
 				boxUnityChan = GameObject.Find("BoxUnityChan");
 				subCamera.enabled = false;
+				isFirstPersonCamera = false;
+				defaultEulerAngles = transform.eulerAngles;
 		}
 	
 		// Update is called once per frame
-		void FixedUpdate () {
+		void FixedUpdate ()
+		{
 
+				float h = Input.GetAxis ("Horizontal"); // 入力デバイスの水平軸をhで定義
+				float v = Input.GetAxis ("Vertical");	 // 入力デバイスの垂直軸をvで定義
+				
+				if (isFirstPersonCamera) {
+						
+						if(Input.GetKeyUp (KeyInputManager.cameraFirstPersonKeyCode)) {
+						transform.eulerAngles = defaultEulerAngles;
+						transform.localPosition = new Vector3(0.0f, 0.0f, 3.0f);
+						isFirstPersonCamera = false;
+						unityChan.SendMessage("SetMoveEnabled", true);
+						boxUnityChan.SendMessage("SetMoveEnabled", true);
+					}
+						
+					Vector3 nr = transform.eulerAngles;
+					transform.eulerAngles = new Vector3 (nr.x - 3.0f * v, nr.y + 3.0f * h, nr.z);
+		}
+				
 				// change camera 切り替え
 				if (Input.GetKeyDown(KeyInputManager.changeCharacterKeyCode)) {
 						if(mainCamera.enabled){
@@ -32,10 +54,12 @@ public class CameraScript : MonoBehaviour {
 				} 
 			
 				// 主眼カメラ
-				if(Input.GetKey (KeyInputManager.cameraFirstPersonKeyCode)) {
+				if(Input.GetKeyDown (KeyInputManager.cameraFirstPersonKeyCode) && h == 0 && v == 0) {
 						transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
-				}else{
-					 transform.localPosition = new Vector3(0.0f, 0.0f, 3.0f);
+						defaultEulerAngles = transform.eulerAngles;
+						isFirstPersonCamera = true;
+						unityChan.SendMessage("SetMoveEnabled", false);
+						boxUnityChan.SendMessage("SetMoveEnabled", false);
 				}
 						
 		}
