ce09e8d83fcd92cbfabfcb3c7f94738906c00674
lethe <lethe2211@gmail.com>
2014-10-20 17:48:16 +0900

    StageFlagManagerのフラグが複数ボタンに対応していなかったので直した

--- UnityChanEscape/Assets/Scripts/Utility/StageFlagManager.cs
+++ UnityChanEscape/Assets/Scripts/Utility/StageFlagManager.cs
@@ -25,22 +25,35 @@ public class StageFlagManager : MonoBehaviour {
 				} else {
 						currentFlags [sender] = true;
 				}
+//				Debug.Log (sender.name);
+//				Debug.Log (currentFlags[sender]);
 
 				// トリガーの発火
 				// TargetとなるGameObjectにTriggerOnメッセージを送る
 				foreach (TriggerTarget item in triggerTargets) {
 						List<Trigger> trgrs = item.triggers;
-						bool triggerOn = true;
+						bool triggerOn = false; // 最終的にフラグの変更をターゲットに通知するかどうか
+
+						bool triggerMatched = true; // トリガーの発火に必要な条件をすべて満たしているかどうか
+						bool containedSenderInTrigger = false; // トリガーの発火に必要な条件にFlagChangedメッセージを送ってきたGameobjectが含まれているかどうか
+						// これを考慮しないと，複数のボタンがあるときに，片方のボタンで設定したトリガーが，もう片方のボタンのフラグ変更によって再度呼ばれてしまう
+
 						foreach (Trigger t in trgrs) {
 //								Debug.Log (t.button.name);
 //								Debug.Log (t.flag);
 //								Debug.Log (currentFlags.ContainsKey (t.button));
 //								Debug.Log(currentFlags[t.button]);
+								if (t.button == sender)
+										containedSenderInTrigger = true;
 								if (!currentFlags.ContainsKey(t.button) || currentFlags [t.button] != t.flag) {
-										triggerOn = false;
+										triggerMatched = false;
 										break;
 								}
 						}
+
+						// 上の両方が満たされたときにフラグの変更を通知する
+						if (triggerMatched && containedSenderInTrigger)
+								triggerOn = true;
 						if (triggerOn) {
 								// Debug.Log ("TriggerOn");
 								item.target.SendMessage ("TriggerOn");
