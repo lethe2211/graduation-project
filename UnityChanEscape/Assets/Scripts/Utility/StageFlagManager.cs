2afc31393592e9bee96d58ab9254e6e4acac755b
lethe <lethe2211@gmail.com>
2014-10-20 15:49:43 +0900

    StageFlagManager.csのcurrentFlagsのキーをGameObjectクラスのオブジェクト に，直した．あと条件判定が間違ってたのを修正した

--- UnityChanEscape/Assets/Scripts/Utility/StageFlagManager.cs
+++ UnityChanEscape/Assets/Scripts/Utility/StageFlagManager.cs
@@ -6,7 +6,7 @@ using System.Linq;
 // ステージごとのフラグを管理し，必要に応じてフラグの値の変更，変更の通知を行うクラス
 [System.Serializable]
 public class StageFlagManager : MonoBehaviour {
-		Dictionary<string, bool> currentFlags = new Dictionary<string, bool>(); // 現在のGameObjectとフラグの組．FlagChangedが呼ばれないとaddされないことに注意
+		Dictionary<GameObject, bool> currentFlags = new Dictionary<GameObject, bool>(); // 現在のGameObjectとフラグの組．FlagChangedが呼ばれないとaddされないことに注意
 		public List<TriggerTarget> triggerTargets; // 
 
 		// Use this for initialization
@@ -17,29 +17,32 @@ public class StageFlagManager : MonoBehaviour {
 		// ステージ中の仕掛けのフラグの値が変わる度に呼ばれる
 		void FlagChanged(GameObject sender) {
 				// Debug.Log ("FlagChanged");
-				string name = sender.name; // 呼び出し元GameObjectの名前
+				// string name = sender.name; // 呼び出し元GameObjectの名前
 
 				// フラグ変更
-				if (currentFlags.ContainsKey (name)) {
-						currentFlags [name] = !currentFlags [name];
+				if (currentFlags.ContainsKey (sender)) {
+						currentFlags [sender] = !currentFlags [sender];
 				} else {
-						currentFlags [name] = true;
+						currentFlags [sender] = true;
 				}
 
 				// トリガーの発火
 				// TargetとなるGameObjectにTriggerOnメッセージを送る
 				foreach (TriggerTarget item in triggerTargets) {
 						List<Trigger> trgrs = item.triggers;
-						bool flg = true;
+						bool triggerOn = true;
 						foreach (Trigger t in trgrs) {
 //								Debug.Log (t.button.name);
 //								Debug.Log (t.flag);
-								if (!currentFlags.ContainsKey(t.button.name) || currentFlags [t.button.name] == t.flag) {
-										flg = false;
+//								Debug.Log (currentFlags.ContainsKey (t.button));
+//								Debug.Log(currentFlags[t.button]);
+								if (!currentFlags.ContainsKey(t.button) || currentFlags [t.button] != t.flag) {
+										triggerOn = false;
 										break;
 								}
 						}
-						if (flg) {
+						if (triggerOn) {
+								// Debug.Log ("TriggerOn");
 								item.target.SendMessage ("TriggerOn");
 						}
 				}
