1bcb8c9c81b02c483452c4950e9defe7e7e2cae1
yoshikyoto <admin@h96.26.238.10.30790.vlan.kuins.net>
2014-11-18 14:20:51 +0900

    add sakamoto stage1-3

---
+++ UnityChanEscape/Assets/Scripts/StageObject/ElevatorButtonScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElevatorButtonScript : MonoBehaviour {
+
+	public Color trueColor;
+	private Color falseColor;
+	private Behaviour halo;
+	public GameObject targetObj;
+	public float moveDirection;
+
+	private ElevatorWithButtonScript targetComponent;
+	private int objectCount = 0;
+
+	// Use this for initialization
+	void Start () {
+		falseColor = renderer.material.color;
+		halo = (Behaviour)gameObject.GetComponent("Halo");
+
+		targetComponent = targetObj.GetComponent<ElevatorWithButtonScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(objectCount > 0){
+			renderer.material.color = trueColor;
+			halo.renderer.material.color = trueColor;
+			targetComponent.AddMoveDirection(moveDirection);
+			//halo.enabled = false;
+		}else{	
+			renderer.material.color = falseColor;
+		}
+	}
+
+	void OnCollisionEnter(Collision collision){
+		string name = collision.gameObject.name;
+		if(name.IndexOf("Plage") < 0){
+			objectCount++;
+		}
+	}
+
+	
+	void OnCollisionExit(Collision collision){
+		string name = collision.gameObject.name;
+		if(name.IndexOf("Plage") < 0){
+			objectCount--;
+		}
+	}
+}
