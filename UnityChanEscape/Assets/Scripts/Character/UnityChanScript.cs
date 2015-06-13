4664b849f0627de7cbbd15633ef0034a981eee10
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2015-06-13 17:11:25 +0900

    Character周りのコードにざっとコメントを追加してリファクタリングした
    	modified:	BUnityChanScript.cs
    	modified:	CharacterScript.cs
    	modified:	UnityChanScript.cs

--- UnityChanEscape/Assets/Scripts/Character/UnityChanScript.cs
+++ UnityChanEscape/Assets/Scripts/Character/UnityChanScript.cs
@@ -1,132 +1,156 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/**
+ * ユニティちゃんを制御するためのスクリプト
+ *
+ * 重力に関しては、キャラによって重力の方向が違うため、
+ * Unityデフォルトの重力機能は使っていない
+ */
 public class UnityChanScript : CharacterScript {
-	
-	private bool gAlreadyPushed = false;
-	private int gChangeFrame = 190;
-	private bool cleared = false;
-	private bool gameOverFlag = false;
+    
+    private bool gAlreadyPushed = false;
+    private int gChangeFrame = 190;
+    private bool cleared = false;
+    private bool gameOverFlag = false;
 
-	GameObject gameOverCameraObject;
-	Camera gameOverCamera;
-	GameObject gameClearObject;
-	GameObject gameOverObject;
-	
-	protected GameObject mainCameraHorizontalObject;
-	public float gameOverPosition;
+    GameObject gameOverCameraObject;
+    Camera gameOverCamera;
+    GameObject gameClearObject;
+    GameObject gameOverObject;
+    
+    protected GameObject mainCameraHorizontalObject;
 
-	// Use this for initialization
-	void Start () {
-		base.Start ();
-		mainCameraHorizontalObject = GameObject.Find("MainCameraHorizontalObject");
-		
-		gameOverCameraObject = GameObject.Find("GameOverCamera");
-		gameOverCamera = gameOverCameraObject.GetComponent<Camera>();
-		gameOverCamera.enabled = false;
+    // この座標よりも小さくなったらゲームオーバーと判定する
+    public float gameOverPosition;
 
-		cameraObject = GameObject.Find ("MainCameraHorizontalObject");
-		rotationZ = 0;
+    /**
+     * 各種値の初期化を行う
+     */
+    void Start ()
+    {
+        base.Start ();
+        mainCameraHorizontalObject = GameObject.Find("MainCameraHorizontalObject");
+        
+        gameOverCameraObject = GameObject.Find("GameOverCamera");
+        gameOverCamera = gameOverCameraObject.GetComponent<Camera>();
+        gameOverCamera.enabled = false;
 
-		gameOverObject = GameObject.Find ("GameOverObject");
-		gameClearObject = GameObject.Find ("GameClearObject");
-	}
+        cameraObject = GameObject.Find ("MainCameraHorizontalObject");
+        rotationZ = 0;
 
-
-	void Update ()
-		{
-				// カメラが有効な時だけ動く 
-				if (!mainCamera.enabled)
-						return;
-				// パテマとかで動きが制限されている場合
-				if (!moveEnabled) {
-						cameraObject.transform.position = transform.position + transform.up;
-						return;
-				}
-
-		base.Update ();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-
-		// gravity
-		if(gravityEnabled)rigidbody.AddForce (unityChan.transform.up * rigidbody.mass * -7);
-
-		// GameOver
-		if(gameOverFlag){
-			transform.Rotate(0, 1, 0);
-			return;
-		}
-		if(transform.position.y <= gameOverPosition){
-			GameOver();
-			gameOverFlag = true;
-			return;
-		}
-
-		// if camera is selected, you can move unity-chan
-		animator.SetBool("isRunning", false);
-		if(!mainCamera.enabled) return;
-		if(!moveEnabled){
-			print ("unity-chan ugokenai");
-			// Vector3 p = new Vector3(-0.05f, 3.0f, 0.0f);
-			// transform.localPosition = p;
-			return;
-		}
-
-		// flag for stage clear
-		if(cleared){
-			if(animator.GetBool("Clear")) return;
-			animator.SetBool("Clear", true);
-						mainCameraHorizontalObject.transform.position = transform.position + transform.up / 2;
-			mainCameraHorizontalObject.transform.forward = transform.forward;
-			return;
-		}else{
-			animator.SetBool("Clear", false);
-		}
-
-		base.Move();
-
-		// reset camera position
-		if(Input.GetKey("g")){
-			if(!gAlreadyPushed){
-				Physics.gravity *= -1;
-				gChangeFrame = 0;
-			}
-			gAlreadyPushed = true;
-		}else{
-			gAlreadyPushed = false;
-		}
-		gChangeFrame++;
-		if(gChangeFrame <= 90){
-			transform.Rotate(0, 0, 2); 
-		}
-	}
-	
-	protected void OnCollisionEnter(Collision collision){
-		print ("UnityChanScript.OnCollisionEnter");
-		base.OnCollisionEnter (collision);
-		GameObject obj = collision.gameObject; // object of collision
-
-		string name = collision.gameObject.name;
-		if(name == "Goal"){
-			cleared = true;
-			gameClearObject.SendMessage ("Cleared");
-			Destroy(collision.gameObject);
-		}
-
-	}
+        gameOverObject = GameObject.Find ("GameOverObject");
+        gameClearObject = GameObject.Find ("GameClearObject");
+    }
 
 
-	void GameOver(){
-		print ("Game Over");
-		gameOverObject.SendMessage("Over");
-		animator.SetBool ("Fall", true);
-		Vector3 v = transform.position;
-		gameOverCameraObject.transform.position = new Vector3(v.x, -3, v.z);
-		mainCamera.enabled = false;
-		subCamera.enabled = false;
-		gameOverCamera.enabled = true;
-		// gameOverCamera.SendMessage("fadeOut");
-	}
+    /**
+     * ユニティちゃん操作の時のみUpdateが働く
+     *
+     * mainCamera.enabled == true の時はユニティちゃん操作であると判定している
+     */
+    void Update ()
+    {
+        // ユニティちゃん操作でないなら以下実行されない
+        if (!mainCamera.enabled)
+            return;
+        // パテマとかで動きが制限されている場合
+        if (!moveEnabled) {
+            cameraObject.transform.position = transform.position + transform.up;
+            return;
+        }
+
+        base.Update ();
+    }
+    
+
+    /**
+     * キャラの移動や重力まわり
+     */
+    void FixedUpdate ()
+    {
+        // gravityEnabled = true の時のみ下方向に重力をかける
+        if(gravityEnabled)rigidbody.AddForce (unityChan.transform.up * rigidbody.mass * -7);
+
+        // ゲームオーバーまわり
+        if(gameOverFlag){
+            transform.Rotate(0, 1, 0);
+            return;
+        }
+        if(transform.position.y <= gameOverPosition){
+            GameOver();
+            gameOverFlag = true;
+            return;
+        }
+
+        // スティックを倒したままキャラチェンジしてもRunningアニメーションが切れるように一度falseにする
+        animator.SetBool("isRunning", false);
+        // unity-chanが操作可能な条件
+        if(!mainCamera.enabled) return;
+        if(!moveEnabled){
+            return;
+        }
+
+        // ステージクリアまわり
+        if(cleared){
+            if(animator.GetBool("Clear")) return;
+            animator.SetBool("Clear", true);
+            mainCameraHorizontalObject.transform.position = transform.position + transform.up / 2;
+            mainCameraHorizontalObject.transform.forward = transform.forward;
+            return;
+        }else{
+            animator.SetBool("Clear", false);
+        }
+
+        // キー入力による動作など、ボックスユニティちゃんと共通の部分をおこなう
+        base.Move();
+
+        // gボタンでカメラリセットを行う
+        // TODO: キーコンフィグ専用クラスを使う
+        if(Input.GetKey("g")){
+            if(!gAlreadyPushed){
+                Physics.gravity *= -1;
+                gChangeFrame = 0;
+            }
+            gAlreadyPushed = true;
+        }else{
+            gAlreadyPushed = false;
+        }
+        gChangeFrame++;
+        if(gChangeFrame <= 90){
+            transform.Rotate(0, 0, 2); 
+        }
+    }
+
+    /**
+     * 何かに触れた時
+     */
+    protected void OnCollisionEnter(Collision collision){
+        base.OnCollisionEnter (collision);
+        GameObject obj = collision.gameObject;
+        string name = collision.gameObject.name;
+
+        // ゴールに触れた時はゴール
+        if(name == "Goal"){
+            cleared = true;
+            gameClearObject.SendMessage ("Cleared");
+            Destroy(collision.gameObject);
+        }
+
+    }
+
+
+    /**
+     * ゲームオーバー時の処理
+     */ 
+    void GameOver(){
+        gameOverObject.SendMessage("Over");
+        animator.SetBool ("Fall", true);
+        Vector3 v = transform.position;
+        gameOverCameraObject.transform.position = new Vector3(v.x, -3, v.z);
+        mainCamera.enabled = false;
+        subCamera.enabled = false;
+        gameOverCamera.enabled = true;
+        // gameOverCamera.SendMessage("fadeOut");
+    }
 }
