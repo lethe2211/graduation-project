d66d184b1a9d26f57bdfbfa713e84814cdd2b644
peichan <ktgchyshnr13@gmail.com>
2015-06-13 16:01:12 +0900

    Title以下のスクリプトのインデントを修正

--- UnityChanEscape/Assets/Scripts/Title/LicenseManager.cs
+++ UnityChanEscape/Assets/Scripts/Title/LicenseManager.cs
@@ -1,21 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LicenseManager : MonoBehaviour {
+public class LicenseManager : MonoBehaviour
+{
+    public AudioSource voice;
 
-	public AudioSource voice;
-		
-	// Use this for initialization
-	void Start () {
-		voice.Play();
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		if (!voice.isPlaying) {
-			System.Threading.Thread.Sleep(1000);
-			Application.LoadLevel("Title");
-		}
-	}
+    // Use this for initialization
+    void Start()
+    {
+        voice.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!voice.isPlaying)
+        {
+            System.Threading.Thread.Sleep(1000);
+            Application.LoadLevel("Title");
+        }
+    }
 }
