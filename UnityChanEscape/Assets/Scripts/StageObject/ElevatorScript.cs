8713cede88b136390672387aab6d88c5f8fd07a9
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2015-06-13 15:06:53 +0900

    add comment to ElevatorScript.cs

--- UnityChanEscape/Assets/Scripts/StageObject/ElevatorScript.cs
+++ UnityChanEscape/Assets/Scripts/StageObject/ElevatorScript.cs
@@ -1,25 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/**
+ * 上下移動する床のスクリプト
+ */
 public class ElevatorScript : MonoBehaviour {
 
+    // 床の移動に使う情報
 	private float frame = 0;
 	private float stopFrame = 0;
 
+    // 床の最も高い位置のY座標
 	public float upperY;
+    // 床の最も低い位置のY座標
 	public float downerY;
-	public float relativelySpeed;
+    // 相対的
+	public float relativelySpeed; 
 
+    // upperとdownerのちょうど真ん中のY座標
 	private float centerPositionY;
+    // 床の中心からupper,downerの距離
 	private float moveRangeHalf; 
 
-	// Use this for initialization
+    /**
+     * 値の初期化
+     */
 	void Start () {
 		centerPositionY = (upperY + downerY) / 2;
 		moveRangeHalf = upperY - centerPositionY;
 	}
 	
-	// Update is called once per frame
+
+    /**
+     * 1フレームごとに床の位置を再計算させて移動する
+     *
+     * 基本的にはsin関数にしたがって、upperとdownerの間を床が移動するが、
+     * 一番上の位置と一番下の位置では 50 フレーム停止する
+     */
 	void FixedUpdate () {
 		frame += relativelySpeed + 0.01f;
 		float sin = Mathf.Sin(frame);
@@ -27,7 +44,6 @@ public class ElevatorScript : MonoBehaviour {
 		if(sin == 0) frame = 0.0f;
 
 		if (sin == 1 || sin == -1) {
-			 // 一番上と一番下ではちょっと止まる
 			stopFrame++;
 			if(stopFrame <= 50) return;
 		}else{
@@ -39,10 +55,16 @@ public class ElevatorScript : MonoBehaviour {
 		transform.position = new Vector3(p.x, y, p.z);
 	}
 
+    /**
+     * この床に誰かが乗った場合は、そのものも床と一緒に動かす
+     */
 	void OnCollisionEnter(Collision c){
 		c.gameObject.transform.parent = transform;
 	}
 
+    /**
+     * 床から離れたら一緒には動かない
+     */
 	void OnCollisionExit(Collision c){
 		c.gameObject.transform.parent = null;
 	}
