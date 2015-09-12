bb66c40b4275a1d079fbceb5e90a00620a86215f
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2015-09-12 18:54:38 +0900

    getCharacterだとGameObjectを返しそうなのでメソッド名を変えたりした

--- UnityChanEscape/Assets/Scripts/Character/CharacterConst.cs
+++ UnityChanEscape/Assets/Scripts/Character/CharacterConst.cs
@@ -5,7 +5,7 @@ using System.Collections;
  * キャラクターに関する定数
  */
 public class CharacterConst : MonoBehaviour {
-    public const int INVALID = 0;
-    public const int UNITY_CHAN = 1;
-    public const int BOX_UNITY_CHAN = 2;
+    public const int INVALID_ID = 0;
+    public const int UNITY_CHAN_ID = 1;
+    public const int BOX_UNITY_CHAN_ID = 2;
 }
--- UnityChanEscape/Assets/Scripts/Character/CharacterManager.cs
+++ UnityChanEscape/Assets/Scripts/Character/CharacterManager.cs
@@ -3,34 +3,58 @@ using System.Collections;
 
 /**
  * キャラクターの状態とかを管理するクラス
+ *
+ * static っぽいメソッドはこっちに生やして欲しい
  */
 public class CharacterManager : MonoBehaviour {
     protected static Camera mainCamera;
     protected static Camera subCamera;
+    protected static GameObject boxUnityChan;
+    protected static GameObject unityChan;
 
     /**
      * カメラオブジェクトを取得する
      */
-    protected static void InitCameraObject() {
+    protected static void Init() {
         if(mainCamera == null) {
             mainCamera = GameObject.Find("MainCamera").GetComponent<Camera>();
             subCamera = GameObject.Find("SubCamera").GetComponent<Camera>();
+            unityChan = GameObject.Find ("unitychan"); 
+            boxUnityChan = GameObject.Find ("BoxUnityChan");
         }
     }
 
     /**
-     * 操作キャラクターを取得する
+     * 操作キャラクターIDを取得する
+     *
+     * 使用例
+     * if(CharacterManager.GetEnabledCharacterId == CharacterConst.UNITY_CHAN_ID) {
+     *     // ユニティちゃん専用処理
+     * }
      */
-    public static int GetEnabledCharacter() {
-        InitCameraObject();
+    public static int GetEnabledCharacterId() {
+        Init();
         if(mainCamera.enabled) {
-            return CharacterConst.UNITY_CHAN;
+            return CharacterConst.UNITY_CHAN_ID;
         }
-
         if(subCamera.enabled) {
-            return CharacterConst.BOX_UNITY_CHAN;
+            return CharacterConst.BOX_UNITY_CHAN_ID;
         }
-        
-        return CharacterConst.INVALID;
+        return CharacterConst.INVALID_ID;
+    }
+
+    /**
+     * 操作キャラクターを返す
+     */
+    public static GameObject GetEnabledCharacter() {
+        Init();
+        switch(GetEnabledCharacterId()) {
+            case CharacterConst.UNITY_CHAN_ID:
+                return unityChan;
+            case CharacterConst.BOX_UNITY_CHAN_ID:
+                return boxUnityChan;
+            default:
+                return null;
+        }
     }
 }
